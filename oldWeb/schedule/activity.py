f03b7bf8d96a71eaa97e991c96a2dad0504c5bb4
DESKTOP-A04RQ93\yasme <yhmaidan@youthneuro.org>
2019-04-22 19:11:29 -0400

    instantiated repository with basic framework

--- oldWeb/schedule/activity.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Schedule(models.Model):
-    activity_name = models.CharField(max_length=200)
-    activity_date = models.DateField()
-    content = models.TextField()
-
-    def __str__(self):
-        return self.activity_name
--- schooly/docker/gunicorn.sh
+++ schooly/docker/gunicorn.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Waits until Postgres DB is accessible (started in different container)
-while ! curl http://$DB_HOST:$DB_PORT/ 2>&1 | grep '52'
-    do
-        echo 'retry'
-        sleep 5
-    done
+#while ! curl http://$DB_HOST:$DB_PORT/ 2>&1 | grep '52'
+#    do
+#        echo 'retry'
+#        sleep 5
+#    done
+sleep 30
 
 if [ $ENV == "DEV" ]; then
     # We are in development mode
